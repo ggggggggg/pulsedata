85fb5e677f42e30b92c16b75fe3b0fa1647b1b10
ggggggggg <ggggggggg@users.noreply.github.com>
2024-07-25 16:06:24 -0600

    add another test

--- pulsedata/test_data_is_there.py
+++ pulsedata/test_data_is_there.py
@@ -7,3 +7,7 @@ def test_data_is_there():
     for k,v in pulsedata.pulse_noise_ljh_pairs.items():
         assert v.pulse_folder.is_dir()
         assert v.noise_folder.is_dir()
+
+
+def test_can_open_files():
+    open(pulsedata.off["ebit_20240722_0006"]/"20240722_run0006_chan1.off","rb")
